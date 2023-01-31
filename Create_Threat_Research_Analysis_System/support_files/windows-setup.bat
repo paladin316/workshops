08cc9abb41606810cbd1b1549f7301c3a55f4113
paladin316 <paladin31six@gmail.com>
2023-01-31 10:17:53 -0700

    Update windows-setup.bat

--- Create_Threat_Research_Analysis_System/support_files/windows-setup.bat
+++ Create_Threat_Research_Analysis_System/support_files/windows-setup.bat
@@ -1,11 +1,11 @@
 :: Unblock Disable Defender Script and Window Setup Script
-cmd /C powershell.exe Unblock-File T:\workshopsCreate_Threat_Research_Analysis_Systemsupport_files\disable-defender.ps1
-cmd /C powershell.exe Unblock-File T:\workshopsCreate_Threat_Research_Analysis_Systemsupport_files\windows-setup-runonce.ps1
+cmd /C powershell.exe Unblock-File T:\workshops\Create_Threat_Research_Analysis_System\support_files\disable-defender.ps1
+cmd /C powershell.exe Unblock-File T:\workshops\Create_Threat_Research_Analysis_System\support_files\windows-setup-runonce.ps1
 cmd /C powershell.exe Set-ExecutionPolicy Unrestricted -force
 
 :: Run Disable Defender Script and Window Setup Script
-cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshopsCreate_Threat_Research_Analysis_Systemsupport_files\disable-defender.ps1
-cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshopsCreate_Threat_Research_Analysis_Systemsupport_files\windows-setup-runonce.ps1
+cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshops\Create_Threat_Research_Analysis_System\support_files\disable-defender.ps1
+cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshops\Create_Threat_Research_Analysis_System\support_files\windows-setup-runonce.ps1
 
 :: Restart Computer via PowerShell
 cmd /C powershell.exe Restart-Computer
