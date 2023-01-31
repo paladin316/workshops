8f2406caf197ac77b269d7a87fd6210430e6e922
paladin316 <paladin31six@gmail.com>
2023-01-31 11:45:33 -0700

    Update windows-setup.bat

--- Create_Threat_Research_Analysis_System/support_files/windows-setup.bat
+++ Create_Threat_Research_Analysis_System/support_files/windows-setup.bat
@@ -3,7 +3,7 @@ cmd /C powershell.exe Unblock-File T:\workshops\Create_Threat_Research_Analysis_
 cmd /C powershell.exe Unblock-File T:\workshops\Create_Threat_Research_Analysis_System\support_files\windows-setup-runonce.ps1
 cmd /C powershell.exe Set-ExecutionPolicy Unrestricted -force
 
-:: Run Disable Defender Script and Window Setup Script
+:: Run Disable Defender Script and Window Setup Script - Credit for Defender Script - https://github.com/jeremybeaume/tools/blob/master/disable-defender.ps1
 cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshops\Create_Threat_Research_Analysis_System\support_files\disable-defender.ps1
 cmd /C powershell.exe -ExecutionPolicy Bypass -File T:\workshops\Create_Threat_Research_Analysis_System\support_files\windows-setup-runonce.ps1
 
