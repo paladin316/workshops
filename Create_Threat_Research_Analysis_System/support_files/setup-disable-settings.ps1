9151bdb802a9de891dbd2695710a50dfe1172b79
paladin316 <paladin31six@gmail.com>
2023-01-30 20:17:33 -0700

    Delete Create_Threat_Research_Analysis_System/support_files directory

--- Create_Threat_Research_Analysis_System/support_files/setup-disable-settings.ps1
+++
@@ -1,5 +0,0 @@
-# Unblock the installation script by running:
-cmd /C powershell.exe Unblock-File T:\support_files\reg_disable_win_settings.ps1
-# Enable script execution by running:
-cmd /C powershell.exe  Set-ExecutionPolicy Unrestricted -force
-cmd /C powershell.exe  T:\support_files\reg_disable_win_settings.ps1
