e08f0f2587a288173a55fb8028fdc2347b4afdeb
paladin316 <paladin31six@gmail.com>
2023-02-01 16:24:45 -0700

    Update Linux_install_setup.sh

--- Create_Threat_Research_Analysis_System/Linux_install_setup.sh
+++ Create_Threat_Research_Analysis_System/Linux_install_setup.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check to make sure script is not being exectuted as root
 if [ "$(id -u)" -eq 0 ]; then tput bold;tput setaf 5;echo "Please do not run as root." >&2; exit 1; fi
 
-set -e
 Home=$PWD
-reqSpace=200000000
+reqSpace=2000000000
 SPACE=`df "$Home" | awk 'END{print $4}'`
-if [[ $SPACE -le reqSpace ]]
-then
+if [ $SPACE -le $reqSpace ];then
     tput bold;tput setaf 5;echo "Not Enough Disk Space to Continue";tput sgr0
     exit 1
 fi
@@ -22,7 +20,7 @@ git clone https://github.com/paladin316/workshops.git 2>/dev/null &&
 #Remnux Install
 tput bold;tput setaf 5; echo "Downloading and installing Remnux; Grab some coffee, this might take a while";tput sgr0
 cd /home/$USER/tools/
-wget https://REMnux.org/remnux-cli
+#wget https://REMnux.org/remnux-cli
 
 # 23c7f4eefa7599ea2c4156f083906ea5fd99df18f306e4bb43eec0430073985a
 sha256sum remnux-cli
